9dfb21804949217d511d3d2b797a220a3eef7a3a
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-29 20:36:39 -0700

    Actual nondet iterator loop

--- examples/rust/loops/iterator_fail.rs
+++ examples/rust/loops/iterator_fail.rs
@@ -3,10 +3,18 @@ mod smack;
 use smack::*;
 // @flag --no-memory-splitting --unroll=10
 // @expect error
+fn fac(n: u64) -> u64 {
+  match n {
+    0 => 1,
+    1 => 1,
+    _ => n*fac(n-1)
+  }
+}
 fn main() {
   let mut a = 1;
-  for i in 1..7 as u64 {
+  let n = 6u64.nondet();
+  for i in 1..n+1 as u64 {
     a *= i;
   }
-  assert!(a != 720); // a should equal 6!
+  assert!(a != fac(n)); // a should equal 6!
 }
