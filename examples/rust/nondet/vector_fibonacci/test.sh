3622e568bef8b0173fc94ad1d44a3015947d7fb7
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-29 18:49:02 -0700

    Failing example

--- examples/rust/nondet/vector_fibonacci/test.sh
+++ examples/rust/nondet/vector_fibonacci/test.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
+# Run this to make sure the smack mod is present
+smack fibonacci.rs --no-verify
 rustc -A unused-imports -C opt-level=0 -C no-prepopulate-passes -g --emit=llvm-ir --cfg 'verifier="smack"' fibonacci.rs
 clang -S -emit-llvm fibonacci_c.c
 llvm-link fibonacci.ll fibonacci_c.ll -o fibonacci.bc
