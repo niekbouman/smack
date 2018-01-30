add89e68467abeaed6720cc055fb874183baca4a
Mark S. Baranowski <mark.s.baranowski@gmail.com>
2018-01-29 20:11:36 -0700

    Broken generic function

--- examples/rust/generics/generic_function.rs
+++ examples/rust/generics/generic_function.rs
@@ -5,37 +5,51 @@ use smack::*;
 // @flag --no-memory-splitting
 // @expect verified
 
-struct Point {
-  pub x:u64,
-  pub y:u64
+struct Point<T> {
+  pub x:T,
+  pub y:T
 }
 
-trait D {
-  fn double(self) -> Self;
+struct Point3<T> {
+  pub x:T,
+  pub y:T,
+  pub z:T
 }
 
-impl D for u8 {
-  fn double(self) -> Self {
-    2*self
+trait S<T> {
+  fn swap_items(self)->Self;
+}
+
+impl<T> S<T> for Point<T> {
+  fn swap_items(self) -> Point<T> {
+    Point::<T>{x: self.y, y: self.x}
   }
 }
 
-impl D for Point {
-  fn double(self) -> Self {
-    Point { x: 2*self.x, y: 2*self.y }
+impl<T> S<T> for Point3<T> {
+  fn swap_items(self) -> Point3<T> {
+    Point3::<T>{x: self.y, y: self.z, z: self.x}
   }
 }
 
-fn double<T: D>(x: T) -> T {
-  x.double()
+fn swapem<T, U: S<T>>(s: U) -> U {
+  s.swap_items()
 }
 
 fn main() {
-  let a:u8 = 17;
-  let p = Point { x: 3, y: 4 };
-  let b = double(a);
-  let q = double(p);
-  assert!(b == 34);
-  assert!(q.x == 6);
-  assert!(q.y == 8);
+  let x2 = 7i64.nondet();
+  let y2 = 8i64.nondet();
+  let x3 = 1i64.nondet();
+  let y3 = 2i64.nondet();
+  let z3 = 3i64.nondet();
+  let p2 = Point::<i64>{x: x2, y: y2};
+  let p3 = Point3::<i64>{x: x3, y: y3, z: z3};
+
+  let q2 = swapem(p2);
+  let q3 = swapem(p3);
+  assert!(q2.x != y2);
+  // assert!(q2.y == x2);
+  // assert!(q3.x == y3);
+  // assert!(q3.y == z3);
+  // assert!(q3.z == x3);
 }
